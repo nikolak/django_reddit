cd072acd70583d7d9d48f78de79a0f8e44c9d4c0
Nikola Kovacevic <nikolak@outlook.com>
2015-06-24 21:29:23 +0200

    Add ProfileForm form

--- reddit/forms.py
+++ reddit/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from reddit.models import Submission
+from reddit.models import Submission, RedditUser
 
 
 class UserForm(forms.ModelForm):
@@ -25,6 +25,72 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class ProfileForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': "form-control",
+               'id': "first_name",
+               'type': "text"}),
+        min_length=1,
+        max_length=12,
+        required=False
+    )
+
+    last_name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': "form-control",
+               'id': "last_name",
+               'type': "text"}),
+        min_length=1,
+        max_length=12,
+        required=False
+    )
+
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': "form-control",
+               'id': "email",
+               'type': "text"}),
+        required=False
+    )
+
+    display_picture = forms.BooleanField(required=False)
+
+    about_text = forms.CharField(widget=forms.Textarea(
+        attrs={'class': "form-control",
+               'id': "about_me",
+               'rows': "4",
+               }),
+        max_length=500,
+        required=False
+    )
+
+    homepage = forms.CharField(widget=forms.URLInput(
+        attrs={'class': "form-control",
+               'id': "homepage"}),
+        required=False
+    )
+
+    github = forms.CharField(widget=forms.TextInput(
+        attrs={'class': "form-control",
+               'id': "github",
+               'type': "text"}),
+        required=False,
+        max_length=39
+    )
+
+    twitter = forms.CharField(widget=forms.TextInput(
+        attrs={'class': "form-control",
+               'id': "twitter",
+               'type': "text"}),
+        required=False,
+        max_length=15
+    )
+
+    class Meta:
+        model = RedditUser
+        fields = ('first_name', 'last_name', 'email',
+                  'display_picture', 'about_text',
+                  'homepage','github', 'twitter')
 
 
 class SubmissionForm(forms.ModelForm):
