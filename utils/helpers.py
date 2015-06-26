b3cbf179371c289121126d0cb66d9873d740f202
Nikola Kovacevic <nikolak@outlook.com>
2015-06-26 17:05:20 +0200

    Add post_only and get_only decorators

---
+++ utils/helpers.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponseNotAllowed
+
+def post_only(func):
+    def decorated(request, *args, **kwargs):
+        if request.method != 'POST':
+            return HttpResponseNotAllowed(['GET'])
+        return func(request, *args, **kwargs)
+    return decorated
+
+def get_only(func):
+    def decorated(request, *args, **kwargs):
+        if request.method != 'GET':
+            return HttpResponseNotAllowed(['POST'])
+        return func(request, *args, **kwargs)
+    return decorated
