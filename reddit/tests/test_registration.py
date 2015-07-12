22837a0c96a7117db8658b75432d94cb873c7908
Nikola-K <nikolak@outlook.com>
2015-07-12 23:25:27 +0200

    Add POST requests tests

--- reddit/tests/test_registration.py
+++ reddit/tests/test_registration.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from reddit.forms import UserForm
 from django.contrib.auth.models import User
+from reddit.models import RedditUser
+
 
 class RegistrationFormTestCase(TestCase):
 
@@ -42,3 +44,37 @@ class RegistrationFormTestCase(TestCase):
         form = UserForm(data=test_data)
         self.assertEqual(form.errors['username'], [u"A user with that username already exists."])
         self.assertFalse(form.is_valid())
+
+class RegistrationPostTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_valid_data(self):
+        test_data = {'username':'username',
+                     'password':'password'}
+
+        response = self.c.post('/register/', data=test_data)
+        self.assertRedirects(response,'/login/')
+
+        user = User.objects.filter(username=test_data['username']).first()
+        self.assertIsNotNone(user,
+                             msg="User account was not created, but form submission did not fail")
+
+        redditUser = RedditUser.objects.filter(user=user).first()
+        self.assertIsNotNone(redditUser,
+                             msg="User created but not assigned to RedditUser model")
+
+        self.assertTrue(self.c.login(**test_data), msg="User is unable to login.")
+
+    def test_invalid_data(self):
+        test_data = {'username':'invalid_too_long_username',
+                     'password':'_'}
+
+        response = self.c.post('/register/', data=test_data)
+        self.assertEqual(response.status_code, 200,
+                         msg="Form submission failed, but a registration page was not returned again")
+        self.assertIsNone(User.objects.filter(username=test_data['username']).first(),
+                          msg="Invalid user instance created")
+        self.assertFalse(self.c.login(**test_data), msg="Invalid user data can be used to login")
+
+    
