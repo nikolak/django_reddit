dcfe9c69da649365a5e317d9a5bf780848019c59
Nikola-K <nikolak@outlook.com>
2015-07-17 22:36:10 +0200

    Add submit page tests

--- reddit/tests/test_submission.py
+++ reddit/tests/test_submission.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 from reddit.forms import SubmissionForm
+from reddit.models import RedditUser, Submission
 
 
 class TestSubmissionForm(TestCase):
@@ -30,3 +33,50 @@ class TestSubmissionForm(TestCase):
         self.assertEqual(form.errors['url'], [u"Enter a valid URL."])
         self.assertEqual(form.errors['text'], [u"Ensure this value has at most 5000 characters (it has 5001)."])
         self.assertFalse(form.is_valid())
+
+
+class TestSubmissionRequests(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.login_data = {
+            'username': 'submissiontest',
+            'password': 'password'
+        }
+        RedditUser.objects.create(
+            user=User.objects.create_user(**self.login_data)
+        )
+
+    def test_logged_out(self):
+        r = self.c.get(reverse('Submit'))
+        self.assertRedirects(r, "{}?next={}".format(
+            reverse('Login'), reverse('Submit')
+        ))
+
+    def test_logged_in_GET(self):
+        self.c.login(**self.login_data)
+        r = self.c.get(reverse('Submit'))
+        self.assertIsInstance(r.context['form'], SubmissionForm)
+
+    def test_making_a_submission(self):
+        self.c.login(**self.login_data)
+        test_data = {
+            'title': 'submission title',
+            'url': 'http://example.com',
+            'text': 'submission text'
+        }
+        r = self.c.post(reverse('Submit'), data=test_data, follow=True)
+        submission = Submission.objects.filter(**test_data).first()
+        self.assertIsNotNone(submission)
+        self.assertRedirects(r, reverse('Thread', args=(submission.id,)))
+        self.assertContains(r, 'Submission created')
+
+    def test_missing_fields(self):
+        self.c.login(**self.login_data)
+
+        test_data = {
+            'url': 'http://example.com',
+            'text': 'submission text'
+        }
+        r = self.c.post(reverse('Submit'), data=test_data)
+        self.assertNotContains(r, 'Submission created')
+        self.assertContains(r, 'This field is required.')
