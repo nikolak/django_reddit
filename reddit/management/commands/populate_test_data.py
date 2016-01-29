5bb8c8e536c44e50aad57d7ffb86735a68265679
Nikola-K <nikolak@outlook.com>
2016-01-29 18:52:41 +0100

    move functions to class methods instead of everything under one main function

--- reddit/management/commands/populate_test_data.py
+++ reddit/management/commands/populate_test_data.py
@@ -17,58 +17,14 @@ class Command(BaseCommand):
         parser.add_argument('--root_comments', type=int, default=10)
 
     def handle(self, *args, **options):
-        thread_count = options['thread_count']
-        root_comments = options['root_comments']
-
-        def get_random_username(length=6):
-            return ''.join(choice(letters) for _ in range(length))
-
-        random_usernames = [get_random_username() for _ in range(100)]
-
-        def get_random_sentence(min_words=3, max_words=50,
-                                min_word_len=3,
-                                max_word_len=15):
-            sentence = ''
-
-            for _ in range(0, randint(min_words, max_words)):
-                sentence += ''.join(choice(letters)
-                                    for i in
-                                    range(randint(min_word_len, max_word_len)))
-                sentence += ' '
-
-            return sentence
-
-        def get_or_create_author(username):
-            try:
-                user = User.objects.get(username=username)
-                author = RedditUser.objects.get(user=user)
-            except (User.DoesNotExist, RedditUser.DoesNotExist):
-                print("Creating user {}".format(username))
-                new_author = User(username=username)
-                new_author.set_password(username)
-                new_author.save()
-                author = RedditUser(user=new_author)
-                author.save()
-            return author
-
-        def add_replies(root_comment, depth=1):
-            print("Adding comment replies...")
-            if depth > 5:
-                return
-
-            comment_author = get_or_create_author(choice(random_usernames))
-
-            raw_text = get_random_sentence()
-            new_comment = Comment.create(comment_author, raw_text, root_comment)
-            new_comment.save()
-            if choice([True, False]):
-                add_replies(new_comment, depth + 1)
-
-        for _ in range(thread_count):
+        self.thread_count = options['thread_count']
+        self.root_comments = options['root_comments']
+        self. random_usernames = [self.get_random_username() for _ in range(100)]
+        for _ in range(self.thread_count):
             print("Creating new submission.")
-            selftext = get_random_sentence()
-            title = get_random_sentence(max_words=100, max_word_len=10)
-            author = get_or_create_author(choice(random_usernames))
+            selftext = self.get_random_sentence()
+            title = self.get_random_sentence(max_words=100, max_word_len=10)
+            author = self.get_or_create_author(choice(self.random_usernames))
             ups = randint(0, 1000)
             url = None
             downs = int(ups) / 2
@@ -85,12 +41,54 @@ class Command(BaseCommand):
             submission.generate_html()
             submission.save()
 
-            for _ in range(root_comments):
-                comment_author = get_or_create_author(choice(random_usernames))
-                raw_text = get_random_sentence(max_words=100)
+            for _ in range(self.root_comments):
+                comment_author = self.get_or_create_author(choice(self.random_usernames))
+                raw_text = self.get_random_sentence(max_words=100)
                 new_comment = Comment.create(comment_author, raw_text, submission)
                 new_comment.save()
                 another_child = choice([True, False])
                 while another_child:
-                    add_replies(new_comment)
+                    self.add_replies(new_comment)
                     another_child = choice([True, False])
+
+    def get_random_username(self, length=6):
+        return ''.join(choice(letters) for _ in range(length))
+
+    def get_random_sentence(min_words=3, max_words=50,
+                            min_word_len=3,
+                            max_word_len=15):
+        sentence = ''
+
+        for _ in range(0, randint(min_words, max_words)):
+            sentence += ''.join(choice(letters)
+                                for i in
+                                range(randint(min_word_len, max_word_len)))
+            sentence += ' '
+
+        return sentence
+
+    def get_or_create_author(self, username):
+        try:
+            user = User.objects.get(username=username)
+            author = RedditUser.objects.get(user=user)
+        except (User.DoesNotExist, RedditUser.DoesNotExist):
+            print("Creating user {}".format(username))
+            new_author = User(username=username)
+            new_author.set_password(username)
+            new_author.save()
+            author = RedditUser(user=new_author)
+            author.save()
+        return author
+
+    def add_replies(self, root_comment, depth=1):
+        print("Adding comment replies...")
+        if depth > 5:
+            return
+
+        comment_author = self.get_or_create_author(choice(self.random_usernames))
+
+        raw_text = self.get_random_sentence()
+        new_comment = Comment.create(comment_author, raw_text, root_comment)
+        new_comment.save()
+        if choice([True, False]):
+            self.add_replies(new_comment, depth + 1)
