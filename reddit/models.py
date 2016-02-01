9a89deb0e3a4028f3e96c806d7288c3163da2223
Nikola-K <nikolak@outlook.com>
2016-02-01 23:24:56 +0100

    import from the new utils package for models

--- reddit/models.py
+++ reddit/models.py
@@ -5,39 +5,9 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils import timezone
 from mptt.models import MPTTModel, TreeForeignKey
+from django_reddit.utils.model_utils import ContentTypeAware, MttpContentTypeAware
 
 
-class ContentTypeAware(models.Model):
-    def get_content_type(self):
-        """:return: Content type for this instance."""
-        return ContentType.objects.get_for_model(self)
-
-    def get_content_type_id(self):
-        """:return: Content type ID for this instance"""
-        return self.get_content_type().pk
-
-    def add_vote(self, vote_value):
-        self.score += vote_value
-        if vote_value == 1:
-            self.ups += 1
-        elif vote_value == -1:
-            self.downs += 1
-
-    class Meta:
-        abstract = True
-
-
-class MttpContentTypeAware(MPTTModel):
-    def get_content_type(self):
-        """:return: Content type for this instance."""
-        return ContentType.objects.get_for_model(self)
-
-    def get_content_type_id(self):
-        """:return: Content type ID for this instance"""
-        return self.get_content_type().pk
-
-    class Meta:
-        abstract = True
 
 class Submission(ContentTypeAware):
     author_name = models.CharField(null=False, max_length=12)
